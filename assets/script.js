61b18c961eb620d087d26eb0aef2c52f6eb33b3d
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-04-22 23:28:51 +0200

    Adjust hero swiper styles for better responsiveness and comment out autoplay functionality

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".heroSwiper", {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+    //autoplay: {
+     // delay: 2500,
+     // disableOnInteraction: false,
+    //},
     loop:true,
 
     
